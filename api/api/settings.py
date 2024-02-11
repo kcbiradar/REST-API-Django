f95f20fe1e89439685608bfbb46d28cf773b914a
kcbiradar <khajabiradar360@gmail.com>
2024-02-11 13:52:53 +0530

    developing frontend side

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo_api',
-    'rest_framework'
+    'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
