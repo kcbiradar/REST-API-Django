e4a747749ce6326001fdc99bb027c72829d7ffbd
kcbiradar <khajabiradar360@gmail.com>
2024-02-12 12:56:19 +0530

    ajax request

--- api/todo_api/models.py
+++ api/todo_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    done = models.BooleanField()
+    done = models.BooleanField(default=False,blank=True,null=True)
 
     def __str__(self):
         return self.name
